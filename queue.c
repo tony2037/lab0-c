43428dbf335a5ff9c6156a0b1c7d062b44311ccf
tony2037 <zyex030640417@gmail.com>
2019-02-19 15:49:33 +0800

    If q is NULL, false

--- queue.c
+++ queue.c
@@ -51,6 +51,8 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if (q == NULL)
+        return false;
     list_ele_t *newh;
     /* What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
